d432cfa19993ce33c7d97f388d29f16b3b7a2e5b
Omikadam2620 <163283347+Omikadam2620@users.noreply.github.com>
2024-03-13 16:24:17 +0530

    added one sysout

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.err.println("Failed Deployment for second time");
 		Contoller contoller = new Contoller();
 		contoller.name();
+		System.out.println("change lotty file");
 	}
 	
 }
