e5950f10cb04075e3b7df617181eec2eae253eb4
Omikadam2620 <163283347+Omikadam2620@users.noreply.github.com>
2024-03-13 14:51:02 +0530

    Initial commit

---
+++ src/Contoller.java
@@ -0,0 +1,9 @@
+
+public class Contoller {
+	
+          public void name() {
+        	  System.out.println("Student name is");
+			
+		}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static void main(String arg[]) {
+		
+		System.out.println("Deployed");
+		System.err.println("Failed Deployment for second time");
+		Contoller contoller = new Contoller();
+		contoller.name();
+	}
+	
+}
